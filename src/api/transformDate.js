bef85a7de5f6b5620f3811de707f40ef0adf0e04
StarFishing <2923627211@qq.com>
2019-04-16 16:14:20 +0800

    新增GMT格式时间转正常时间格式接口

---
+++ src/api/transformDate.js
@@ -0,0 +1,16 @@
+export function GMTToStr (time) {
+  let date = new Date(time)
+  let Str =
+    date.getFullYear() +
+    '-' +
+    (date.getMonth() + 1) +
+    '-' +
+    date.getDate() +
+    ' ' +
+    date.getHours() +
+    ':' +
+    date.getMinutes() +
+    ':' +
+    date.getSeconds()
+  return Str
+}
